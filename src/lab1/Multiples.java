cf2a44a1af9d3cefe02a73e33546dca2096baf0d
unknown <naila.anindya.rusdi@gmail.com>
2025-09-08 14:48:09 -0400

    refactored method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,19 +2,23 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-       int howMany = 0;
-       int i = 1;
+        multiples();
+    }
 
-       while (i < 1000) {
-           boolean divisibleBy3 = i % 3 == 0;
-           boolean divisibleBy5 = i % 5 == 0;
+    private static void multiples() {
+        int limit = 1000;
+        int howMany = 0;
+        int i = 1;
 
-           if (divisibleBy3 || divisibleBy5) {
-              howMany++;
-           }
-
-           i++;
-       }
-       System.out.println(howMany);
+        while (i < limit) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+ 
+            if (divisibleBy3 || divisibleBy5) {
+               howMany++;
+            }
+            i++;
+        }
+        System.out.println(howMany);
     }
 }
